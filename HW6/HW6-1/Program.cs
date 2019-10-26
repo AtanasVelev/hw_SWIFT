5354e35b61b5d5b7c39dea2ff75df6c11631e89f
AtanasVelev <atanasgeorgievvelev@gmail.com>
2019-10-26 13:59:47 +0300

    Add homework 6

---
+++ HW6/HW6-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1: Напишете метод, който да приема като параметри име и възраст на човек и да ги отпечати на козолата
+            //Извикайте дадения метод няколко пъти с различни данни.
+
+            GetNameAndAgeOfUser("Ivan",32);
+            GetNameAndAgeOfUser("Pesho", 35);
+            GetNameAndAgeOfUser("Mariika", 43);
+            GetNameAndAgeOfUser("Lybo", 21);
+        }
+
+        public static void GetNameAndAgeOfUser(string name, int age)
+        {
+            Console.WriteLine($"Hello,{name}! Your age is {age}");
+        }
+    }
+}
---
+++ HW6/HW6-4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6_4
+{
+    class Program
+    {
+        //Да се състави програма, която извежда сумата на всички числа от интервала 1 до въведено от конзолата
+        //число.Числото трябва да е в интервала[10 – 100].
+        //Използвайте рекурсия за решението на задачата.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 10-100:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 10 || n > 100)
+            {
+                Console.WriteLine("Your number is out of range");
+            }
+            else
+            {
+                Console.WriteLine(SumOfNumbers(n));
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static int SumOfNumbers(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n + SumOfNumbers(n - 1);
+        }
+
+
+    }
+}
