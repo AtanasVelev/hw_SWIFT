e048486216e158a8c21b245aec8416d07930a82d
AtanasVelev <atanasgeorgievvelev@gmail.com>
2019-11-28 12:04:42 +0200

    HW13

---
+++ HW13/HW13-5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW13_5
+{
+    class Program
+    {
+        //5. Създайте си структурата опашка и я напълнете с числата от 1 до 10.
+        //След това обходете опашката и изтрийте елементите, докато текущия елемент не стане равен на числото 5. Когато стане 5 принтирайте, 
+        //че това е което търсите и спрете обхождането.
+
+
+        static void Main(string[] args)
+        {
+            Queue<int> newQueue = new Queue<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                newQueue.Enqueue(i);
+            }
+
+            //foreach (int num in newQueue)
+            //{
+            //    Console.WriteLine(num);
+            //}
+            int index = 0;
+            while(newQueue.Count>0)
+            {
+                int current = newQueue.Dequeue();
+                index++;
+                if (current==5)
+                {
+                    Console.WriteLine("Job is done!");
+                    return;
+                }
+                Console.WriteLine(current);
+            }
+            foreach (int num in newQueue)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+
+}
---
+++ HW13/HW13-7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW13_7
+{
+    class Program
+    {
+        ////7. Напишете програма, която маха определени думи (по-къси от 3 символа) от изречение Използвайте опашка.               
+        //Пример: My spaceship is ready for travelling to Mars
+        //Решение: spaceship ready travelling Mars
+        static void Main(string[] args)
+        {
+            Queue<string> newQueue = new Queue<string>();
+
+            newQueue.Enqueue("My");
+            newQueue.Enqueue("spaceship");
+            newQueue.Enqueue("is");
+            newQueue.Enqueue("ready");
+            newQueue.Enqueue("for");
+            newQueue.Enqueue("travelling");
+            newQueue.Enqueue("to");
+            newQueue.Enqueue("Mars");
+            
+                int index = 0;
+                
+                while (newQueue.Count>0)
+                {
+                    
+                    if (newQueue.ElementAt(index).ToString().Length > 3)
+                    {
+                        Console.WriteLine(newQueue.ElementAt(index));
+                    }
+
+                    newQueue.Dequeue();
+
+                }
+           
+           
+        }
+
+    
+}
+}
