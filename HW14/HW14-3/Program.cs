d5564e1963a49f519bd757d73eb4e7bc7f3deaa2
AtanasVelev <atanasgeorgievvelev@gmail.com>
2019-12-04 00:11:50 +0200

    hw14

---
+++ HW14/HW14-3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14_3
+{
+    //3. Напишете програма, която приема текст и извежда на екрана всички думи, които са уникални (срещат се само веднъж).
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            text = "I have a dog. It is very good dog. I love my dog so much. The dog is a great dog.";
+
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+            {
+                int count = 1;
+                if (wordsCount.ContainsKey(word))
+                {
+                    count = wordsCount[word] + 1;
+                    
+                }
+                wordsCount[word] = count;
+
+            }
+            foreach (var pair in wordsCount)
+            {
+                if (pair.Value == 1)
+                {
+                    Console.WriteLine($"{pair.Key} - > {pair.Value}");
+                }
+                else continue;
+            }
+
+        }
+    
+    }
+}
---
+++ HW14/HW14-4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14_4
+{
+    class Program
+    {
+//4. Нека имаме дадения стринг:
+//"My name {Ivan} and my dog {Sharo}”
+//Направете програмa, която да проверява дали броя на съдържащите се отварящи скоби е равен на броя на затварящите се в дадения стринг.
+//        След това обхождате всеки елемент на масива и проверявате дали съдържа отваряща скоба - ако да, пъхате в стека, ако съдържа затваряща - махате от стека.
+//        Ако се опитате да махнете от скета, елемент докато там няма накакъв, ще получите exception.
+//        Накрая, ако скобите са равен брой , Count-a на стека трябва да е 0.
+
+//След като я направите програмата, проверете дали работи добре като промените изречението с някакво друго, примерно с НЕ равен брой отварящи и затварящи скоби.
+
+
+
+
+        static void Main(string[] args)
+        {
+            string text = "My name {{{{{Ivan} and my dog {Sharo}";
+            Stack<char> charStack = new Stack<char>();
+
+            char[] letters = text.ToCharArray();
+            foreach(char symbol in letters)
+            {
+                if (symbol == '{')
+                {
+                    charStack.Push(symbol);
+                }
+                if (symbol == '}')
+                {
+                    charStack.Pop();
+                }
+            }
+            Console.WriteLine(charStack.Count);
+        }
+    }
+}
