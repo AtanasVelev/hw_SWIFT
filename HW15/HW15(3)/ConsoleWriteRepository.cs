5d784fc9715071aba3d9d4a29efbb69d38ae3822
AtanasVelev <atanasgeorgievvelev@gmail.com>
2019-12-12 18:42:45 +0200

    HW15

---
+++ HW15/HW15(3)/ConsoleWriteRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW15_3_
+{
+    class ConsoleWriteRepository<T>:IGeneric<T>
+    {
+       public void Create(T entity)
+        {
+            Console.Write($"Create num {entity}");
+        }
+        public void Read(T entity)
+        {
+            Console.Write($"Read num {entity}");
+        }
+        public void Update(T entity)
+        {
+            Console.Write($"Update num {entity}");
+        }
+        public void Delete(T entity)
+        {
+            Console.Write($"Delete num {entity}");
+        }
+    }
+}
